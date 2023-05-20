bdab8dcc3d33d40e47233343ee506ee2b04ad885
yogesh <yogesh.k6499@gmail.com>
2023-05-20 14:27:36 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './styles/_index.scss';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
